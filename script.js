944855ae1701dd7012ea9969b8d8423d50a15dd5
Sarah <soobin.sarah.hwang@gmail.com>
2020-07-06 23:35:51 -0700

    additional changes

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+const generate = document.querySelector("#generate")
 const length = document.querySelector("#characterLength")
 const upper = document.querySelector("#includeUppercase")
 const lower = document.querySelector("#includeLowercase")
 const numbers = document.querySelector("#includeNumbers")
 const specChar = document.querySelector("#includeSpecChar")
 
-let newPassword = document.getElementById('password')
-const UNInum =[48,57];
-const UNIupper = [65,90];
-const UNIlower = [97,122];
-const UNIspecChar = [33,47];
+let newPassword = document.querySelector("#password")
+const upperCharCode = [65, 90];
+const lowerCharCode = [97, 122];
+const numCharCode = [48, 57];
+const specCharCharCode = [33, 47].concat(58, 64).concat(91, 96).concat(123, 126);
 
-document.querySelector("#generate").addEventListener('click', function(e) {
+generate.addEventListener("click", function(e) {
     e.preventDefault();
     const length = characterLength.value;
     const upper = includeUppercase.checked;
@@ -18,32 +19,34 @@ document.querySelector("#generate").addEventListener('click', function(e) {
     const numbers = includeNumbers.checked;
     const specChar = includeSpecChar.checked;
 
-    const randSelector = [];
+    const arraySelector = [];
     const password = [];
-   
+  
+    if(lower===true){
+      for(let i=lowerCharCode[0]; i<= lowerCharCode[1]; i++){
+        arraySelector.push(i);
+      }
+    }
     if(upper===true){
-      for(let i=UNIupper[0]; i<= UNIupper[1]; i++){
-        randSelector.push(i);
+      for(let i=upperCharCode [0]; i<= upperCharCode [1]; i++){
+        arraySelector.push(i);
       }
     }
     if(numbers===true){
-      for(let i=UNInum[0]; i<= UNInum[1]; i++){
-        randSelector.push(i);
-      }
+      for(let i=numCharCode[0]; i<= numCharCode[1]; i++){
+        arraySelector.push(i);
+      } 
     }
     if(specChar===true){
-      for(let i=UNIspecChar[0]; i<= UNIspecChar[1]; i++){
-        randSelector.push(i);
+      for(let i=specCharCharCode[0]; i<= specCharCharCode[1]; i++){
+        arraySelector.push(i);
       }
     }
-    if(lower===true){
-      for(let i=UNIlower[0]; i<= UNIlower[1]; i++){
-        randSelector.push(i);
-      }
-    }
-    
     for(let i = 0; i< length; i++){
-      password.push(String.fromCharCode(randSelector[Math.floor(Math.random()*randSelector.length)]))
+      password.push(String.fromCharCode(arraySelector[Math.floor(Math.random()*arraySelector.length)]))
     }
     newPassword.textContent = password.join("");
   })
+
+  
+      
